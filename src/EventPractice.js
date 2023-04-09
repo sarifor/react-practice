402c38a670bed25dfcc95ac249ef988d75401dcc
sarifor <iread1q84@gmail.com>
2023-04-09 14:37:19 +0900

    Set e.target.value into state

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+    state = { // Why do not use constructor for state in class component?
+        message: ''
+    }
+
     render() {
         return (
             <div>
@@ -9,9 +13,12 @@ class EventPractice extends Component {
                     type="text"
                     name="message"
                     placeholder="Write Anything you want"
+                    value={this.state.message}                
                     onChange={ // Whenever write something, the value will be shown on Console Log
                         (e) => {
-                            console.log(e.target.value);
+                            this.setState({
+                                message: e.target.value
+                            })
                         }
                     }
                 />
