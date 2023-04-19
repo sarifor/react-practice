798013d1ff40a4614a8b4b7d42bfacb14c0fa7f5
sarifor <iread1q84@gmail.com>
2023-04-19 14:44:03 +0900

    Try using .env object in codes in safer way

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import EventPractice from './EventPractice';
 class App extends Component {
 
     _test = () => {
-        // const key = process.env.REACT_APP_KEY // Value is stored in GitHub Actions
         console.log(process.env.REACT_APP_KEY);
-        // console.log(key); // It's just an arbitrary setence for test commit
-
-        /* if (key === "testtest") {
-            console.log(true);
-        } else {
-            console.log(false);
-        } */
     }
 
     render() {
