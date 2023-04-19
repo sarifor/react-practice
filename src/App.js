36bf2d42f58da7d324235f806c3d0e2eb06efc6f
sarifor <iread1q84@gmail.com>
2023-04-19 12:29:24 +0900

    Try using .env object in codes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EventPractice from './EventPractice';
 class App extends Component {
 
     _test = () => {
-        const key = process.env.key // Value is stored in GitHub Actions
+        const key = process.env.REACT_APP_KEY // Value is stored in GitHub Actions
         console.log(key); // It's just an arbitrary setence for test commit
 
         if (key === "testtest") {
